ad4f039e48d145e09d2a31ea11723d274a656708
99002767 <72495787+99002767@users.noreply.github.com>
2020-10-08 09:01:04 +0530

    Add files via upload

---
+++ src/function.c
@@ -0,0 +1,60 @@
+#include<function.h>
+#include<stdio.h>
+#include<math.h>
+
+int add(int input1,int input2)
+{
+    int result = input1+input2;
+    return result;
+}
+
+int subtract(int input1,int input2)
+{
+    int result = input1-input2;
+    return result;
+}
+
+
+int multiply(int input1,int input2)
+{
+    int result = input1*input2;
+    return result;
+}
+
+int divide(int input1,int input2)
+{
+    int result = input1/input2;
+    return result;
+}
+
+double squareroot(double triginput)
+{
+    double result = sqrt(triginput);
+    return result;
+}
+
+int modulus(int input1,int input2)
+{
+    int result = input1 % input2;
+    return result;
+}
+
+int fact(int input1)
+{
+    int i,fact =1;
+    if (input1 < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else {
+        for (i = 1; i <= input1; ++i) {
+            fact *= i;
+        }
+        return fact;
+    }
+}
+
+int power(int input1,int input2)
+{
+    int result = pow(input1,input2);
+    return result;
+}
+
